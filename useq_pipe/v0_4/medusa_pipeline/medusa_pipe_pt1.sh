4390a6f772b27d8700abbca1f39b9e965db96c48
regmgw1 <gareth.wilson@ucl.ac.uk>
2012-02-07 15:17:49 +0000

    Removed hashes

--- useq_pipe/v0_4/medusa_pipeline/medusa_pipe_pt1.sh
+++ useq_pipe/v0_4/medusa_pipeline/medusa_pipe_pt1.sh
@@ -14,16 +14,16 @@ PATH2OUTPUT=$5
 PATH2SCRIPTS=$6
 
 # run fastqc on bam file
-#$FASTQC_HOME/fastqc $PATH2INPUT/$NAME.sorted.bam
+$FASTQC_HOME/fastqc $PATH2INPUT/$NAME.sorted.bam
 # convert to BED
-#echo "$PATH2SCRIPTS/perl_scripts/sam2bedgff.pl $PATH2INPUT/${NAME}_filter.sam $NAME $SPECIES $READ_LENGTH 0 1 $PATH2OUTPUT/$NAME.bed";
-#$PATH2SCRIPTS/perl_scripts/sam2bedgff.pl $PATH2INPUT/${NAME}_filter.sam $NAME $SPECIES $READ_LENGTH 0 1 $PATH2OUTPUT/$NAME.bed 
+echo "$PATH2SCRIPTS/perl_scripts/sam2bedgff.pl $PATH2INPUT/${NAME}_filter.sam $NAME $SPECIES $READ_LENGTH 0 1 $PATH2OUTPUT/$NAME.bed";
+$PATH2SCRIPTS/perl_scripts/sam2bedgff.pl $PATH2INPUT/${NAME}_filter.sam $NAME $SPECIES $READ_LENGTH 0 1 $PATH2OUTPUT/$NAME.bed 
 # generate input file for MEDIPS
-#cut -f 1,2,3,6 $PATH2OUTPUT/$NAME.bed >$PATH2OUTPUT/$NAME.medips
+cut -f 1,2,3,6 $PATH2OUTPUT/$NAME.bed >$PATH2OUTPUT/$NAME.medips
 #run the script in R using Rscript
 $PATH2SCRIPTS/R_scripts/medips_summary.R $NAME $SPECIES $PATH2SCRIPTS $PATH2OUTPUT 
-#gzip $PATH2OUTPUT/${NAME}_rpm.wig
-#gzip $PATH2OUTPUT/${NAME}_for_rpm.wig
-#gzip $PATH2OUTPUT/${NAME}_rev_rpm.wig
+gzip $PATH2OUTPUT/${NAME}_rpm.wig
+gzip $PATH2OUTPUT/${NAME}_for_rpm.wig
+gzip $PATH2OUTPUT/${NAME}_rev_rpm.wig
 #rm $PATH2OUTPUT/$NAME.medips
 
